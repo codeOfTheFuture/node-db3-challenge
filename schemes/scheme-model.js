fd83ebc3581f52740c0bfe01f00eee4ef9a8cc03
Jeff Oliver <jeffoliver3384@gmail.com>
2019-07-17 20:14:55 -0400

    created helper functions for add addstep update and remove

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,10 +4,10 @@ module.exports = {
   find,
   findById,
   findSteps,
-  // add,
-  // addStep,
-  // update,
-  // remove,
+  add,
+  addStep,
+  update,
+  remove,
 };
 
 function find() {
@@ -32,7 +32,7 @@ function findSteps(id) {
     .innerJoin('steps', 'schemes.id', 'steps.scheme_id')
     .where({ scheme_id: id })
     .select(
-      'schemes.id',
+      'steps.id',
       'schemes.scheme_name',
       'steps.scheme_id',
       'steps.step_number',
@@ -40,6 +40,30 @@ function findSteps(id) {
     );
 }
 
-// function add(data) {
+function add(schemeData) {
+  return db('schemes')
+    .insert(schemeData)
+    .then(scheme => console.log(scheme))
+    .catch(err => console.log('Add helper error', err));
+}
 
-// }
+function addStep(stepData, id) {
+  return db('steps')
+    .insert(stepData)
+    .then(step => console.log(step))
+    .catch(err => console.log('Add Step error', err));
+}
+
+function update(changes, id) {
+  return db('schemes')
+    .where('id', Number(id))
+    .update(changes)
+    .then(updateScheme => console.log(updateScheme))
+    .catch(err => console.log('Update scheme err: ', err));
+}
+
+function remove(id) {
+  return db('schemes')
+    .where('id', Number(id))
+    .del();
+}
